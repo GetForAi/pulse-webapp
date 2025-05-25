ac5a353522b37da598c4daa036103aa5f2a5c32a
GetForAi <dmitry.pyatenkov@yandex.ru>
2025-05-25 12:27:56 +0300

    Update state.js

--- src/state.js
+++ src/state.js
@@ -2,5 +2,9 @@ export const appState = {
   telegramId: null,
   firstName: "",
   username: "",
-  xp: 0
+  xp: 0,
+  currentTab: "main",
+  steps: [],
+  rewards: [],
+  avatarUrl: "" // В будущем можно получить из Telegram WebApp или вручную
 };
