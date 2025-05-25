13157787cbdb16bfb0d9d90adcd5569f30ad8744
GetForAi <dmitry.pyatenkov@yandex.ru>
2025-05-25 10:09:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+document.getElementById("startBtn").addEventListener("click", async () => {
+  const steps = ["Создай бота", "Подключи webhook", "Добавь команду", "Ответь пользователю", "Сохрани данные", "Разверни", "Пригласи друга"];
+  const container = document.getElementById("progress");
+  container.innerHTML = "";
+  steps.forEach((step, index) => {
+    const div = document.createElement("div");
+    div.className = "step";
+    div.innerText = `${index + 1}. ${step}`;
+    container.appendChild(div);
+  });
+});
 
