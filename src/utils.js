43b5cec9d10c80d1095fc2237720bad6b2ce2ef0
GetForAi <dmitry.pyatenkov@yandex.ru>
2025-05-25 12:28:49 +0300

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,16 @@
+export function calculateXP(steps) {
+  return steps.length * 10;
+}
 
+export function calculateLevel(xp) {
+  return Math.floor(xp / 50);
+}
+
+export function navigate(path) {
+  history.pushState({}, '', path);
+  window.dispatchEvent(new PopStateEvent('popstate'));
+}
+
+export function getThemeParams() {
+  return window.Telegram?.WebApp?.themeParams || {};
+}
