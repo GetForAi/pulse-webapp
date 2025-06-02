656c4d06e73067df02e8e867189c5f254a92a260
GetForAi <dmitry.pyatenkov@yandex.ru>
2025-06-02 09:28:41 +0300

    Delete src/3d directory

--- src/3d/models.js
+++
@@ -1,9 +0,0 @@
-// Соответствие уровней и путей к 3D-моделям
-export const avatarModels = {
-  1: "/models/level1.glb",
-  2: "/models/level2.glb",
-  3: "/models/level3.glb",
-  4: "/models/level4.glb",
-  5: "/models/level5.glb"
-};
-
